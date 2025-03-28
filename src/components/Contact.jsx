1d72d112716da4ac46d5f9f78d22b39d2b15a7b5
aymanerihane <rihane.aymane@etu.uae.ac.ma>
2025-03-28 02:52:28 +0000

    fix heavy loading

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,69 +1,27 @@
-import React, { useRef, useState } from "react";
+import React, {  useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import emailjs from "@emailjs/browser";
 
 import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
-import { slideIn,textVariant } from "../utils/motion";
-import {FindMe} from '../components'
+import { slideIn, textVariant } from "../utils/motion";
+import { FindMe } from '../components';
 
 const Contact = () => {
-  const formRef = useRef();
-  const [form, setForm] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
+  const [isLargeScreen, setIsLargeScreen] = useState(true);
 
-  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 1280); // Check if the screen width is >= 1024px (lg breakpoint)
+    };
 
-  const handleChange = (e) => {
-    const { target } = e;
-    const { name, value } = target;
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Call it initially to set the right state
 
-    setForm({
-      ...form,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setLoading(true);
-
-    emailjs
-      .send(
-        "service_t0ggo7u",
-        "template_1gmlqfr",
-        {
-          from_name: form.name,
-          to_name: "Aymane Rihane",
-          from_email: form.email,
-          to_email: "rihaneaymanee@gmail.com",
-          message: form.message,
-        },
-        "PBMBCceJ3NH9jt6ZI"
-      )
-      .then(
-        () => {
-          setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
-
-          setForm({
-            name: "",
-            email: "",
-            message: "",
-          });
-        },
-        (error) => {
-          setLoading(false);
-          console.error(error);
-
-          alert("Ahh, something went wrong. Please try again.");
-        }
-      );
-  };
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <div>
@@ -71,76 +29,24 @@ const Contact = () => {
         <p className={`${styles.sectionSubText}`}>Get in touch</p>
         <h2 className={`${styles.sectionHeadText}`}>Contact.</h2>
       </motion.div>
-    
-    <div
-      className={`xl:mt-2 flex xl:flex-row flex-col-reverse items-center w-full gap-10 overflow-hidden`}
-    >
-      
-      <motion.div
-        variants={slideIn("left", "tween", 0.2, 1)}
-        className='flex-[0.75] bg-black-100 p-8 rounded-2xl w-full'
-      >
-        
-        {/* <form
-          ref={formRef}
-          onSubmit={handleSubmit}
-          className='mt-12 flex flex-col gap-8 w-full'
+
+      <div className={`xl:mt-2 flex xl:flex-row flex-col-reverse items-center w-full gap-10 overflow-hidden`}>
+        <motion.div
+          variants={slideIn("left", "tween", 0.2, 1)}
+          className="flex-[0.75] bg-black-100 p-8 rounded-2xl w-full mt-10 lg:mt-0"
         >
-          <label className='flex flex-col '>
-            <span className='text-white font-medium mb-4'>Your Name</span>
-            <input
-              type='text'
-              name='name'
-              value={form.name}
-              onChange={handleChange}
-              placeholder="What's your good name?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-            />
-          </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your email</span>
-            <input
-              type='email'
-              name='email'
-              value={form.email}
-              onChange={handleChange}
-              placeholder="What's your web address?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-            />
-          </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Message</span>
-            <textarea
-              rows={7}
-              name='message'
-              value={form.message}
-              onChange={handleChange}
-              placeholder='What you want to say?'
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium h-[80px]'
-            />
-          </label>
+          <FindMe />
+        </motion.div>
 
-          <button
-            type='submit'
-            className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+        {isLargeScreen && (
+          <motion.div
+            variants={slideIn("right", "tween", 0.2, 1)}
+            className="xl:flex-1 h-[350px] md:h-[550px] w-full lg:w-full"
           >
-            {loading ? "Sending..." : "Send"}
-          </button>
-        </form>
-        <div className="mt-5 justify-center">
-        <h2 className={styles.heroSubText}>OR</h2> */}
-        {/* </div> */}
-
-        <FindMe />
-      </motion.div>
-
-      <motion.div
-        variants={slideIn("right", "tween", 0.2, 1)}
-        className='xl:flex-1 h-[350px] md:h-[550px]  w-full lg:w-full'
-      >
-        <EarthCanvas />
-      </motion.div>
-    </div>
+            <EarthCanvas />
+          </motion.div>
+        )}
+      </div>
     </div>
   );
 };
