47e9a0261ba12c573b4389a073ec2a263ce87ca6
aymanerihane <rihane.aymane@etu.uae.ac.ma>
2025-03-28 01:59:39 +0000

    update contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -73,17 +73,15 @@ const Contact = () => {
       </motion.div>
     
     <div
-      className={`xl:mt-12 flex xl:flex-row flex-col-reverse items-center w-full gap-10 overflow-hidden`}
+      className={`xl:mt-2 flex xl:flex-row flex-col-reverse items-center w-full gap-10 overflow-hidden`}
     >
       
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
         className='flex-[0.75] bg-black-100 p-8 rounded-2xl w-full'
       >
-        <div className="mt-5 justify-center flex">
-        <h2 className={styles.heroSubText}>Contact Me Through the Form</h2>
-        </div>
-        <form
+        
+        {/* <form
           ref={formRef}
           onSubmit={handleSubmit}
           className='mt-12 flex flex-col gap-8 w-full'
@@ -129,9 +127,9 @@ const Contact = () => {
             {loading ? "Sending..." : "Send"}
           </button>
         </form>
-        <div className="mt-5 justify-center flex">
-        <h2 className={styles.heroSubText}>Alternatively, You Can</h2>
-        </div>
+        <div className="mt-5 justify-center">
+        <h2 className={styles.heroSubText}>OR</h2> */}
+        {/* </div> */}
 
         <FindMe />
       </motion.div>
